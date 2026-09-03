2cf0545faeddbaf02409c049fc42cf06dbcb7adf
Hyjin08 <daejung2610@dge.go.kr>
2026-09-03 11:19:49 +0900

    Add files via upload

---
+++ 0903(2).cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp2
+{
+
+    internal class MethodOver
+    {
+        public static int DoubleScale(int val)
+        {
+            return val * 2;
+        }
+        public static float DoubleScale(float val)
+        {
+            return val * 2;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("정수형" + MethodOver.DoubleScale(3));
+            Console.WriteLine("실수형" + MethodOver.DoubleScale(3.14f));
+        }
+    }
+}
---
+++ 0903.cs
@@ -0,0 +1,44 @@
+﻿Single System;
+
+
+
+namespace _0903
+{
+    internal class Program
+    {
+        static void showData()
+        {
+            Console.WriteLine("외부데이터 없이 자체 동작");
+        }
+
+        static int devide(int x, int y)
+        {
+            if(y != 0)
+            {
+                int retval;
+                retval = x/y;
+                Console.WriteLine("{0} {1}을 나눈 값: {2}", x, y, retval);
+                return retval;
+            }
+            else
+            {
+                Console.WriteLine("0으로 나눌 수 없음.");
+                return 0;
+            }
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            showData();
+            devide(7, 3);
+            devide(0, 3);
+            devide(7, 0);
+        }
+    }
+}
+
+
