0125c7fec457b91053ae90492fdf329f663fefd0
Hyjin08 <daejung2610@dge.go.kr>
2026-09-14 11:20:03 +0900

    Add files via upload

---
+++ enum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Enum
+{
+    enum Month
+    {
+        Jan,
+        Fab,
+        March,
+        April,
+        May
+    }
+    public static void enumExe1()
+    {
+        Month my_mon;
+        Month cur_mon;
+
+        my_mon = Month.Jan;
+        cur_mon = (Month)3;
+        Console.WriteLine("달력의 월: {0}, 현재 월: {1}", my_mon, cur_mon);
+        Console.WriteLine("세팅된 월 숫자 {0}", (int)my_mon);
+        Console.WriteLine("현재 월 숫자 {0}", (int)cur_mon);
+    }
+}
